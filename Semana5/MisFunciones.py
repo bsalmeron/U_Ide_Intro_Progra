3a8e6188443b278693f6099efb3ee8f5bf242765
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2022-06-15 19:02:42 -0600

    Cambios

---
+++ Semana5/MisFunciones.py
@@ -0,0 +1,13 @@
+def mostrarDiezElementos():
+    for i in range(5,50,10):
+        print(i)
+
+
+def mostrarNElementos(limite):
+    for i in range(limite):
+        if (i == 5):
+            print("Hola mundo")
+        print(i)
+
+
+def
